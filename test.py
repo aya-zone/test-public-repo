b8b7644c7489ae19ef7d4be6fc56e0d8dd461f00
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 13:52:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ import requests
 password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
-Amazon Secret: 'tx/pgAyk1984bDw3YQw1bpWI+hFU2Ab4GyFOxQ6D
+aws.secretaccesskey: 'tx/pgAyk1984bDw3YQw1bpWI+hFU2Ab4GyFOxQ6D
+key: AKIABAAAAAQAAAAQAAAC
