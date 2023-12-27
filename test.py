2d557a0c7dda2553f8d85ef86aa667c4fe84ab90
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 17:10:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,4 @@ AzureAccountEndpoint=https://<database name>.documents.azure.com:443/;AccountKey
 
 response = requests.get('https://todiaccount56.documents.azure.com:443', {'AccountKey': AzureToken})
 
-'access_token': 'dop_v1_89a302b0583901b006f70a6b755de666b0ec5441ada4fefcb46dec9934785b0b' #AccessToken
-digital_ocean_oauth = 'doo_v1_8476650cb0b595faa44a233f1d579b733248c0061f0d595a836cbce07de41854' #OAuth
-"refresh_token": "dor_v1_d6ce4b93104521c57be0b580e9296453ef4a319b02b5513469f0ec72d89af2e2" 
+CLOJARS_6be7c956ed9581f44c386d3e469ff21e3e4ccb47c83342cfa360f04b435c
