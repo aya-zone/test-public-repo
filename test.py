9af85f34737b0fa08f0999131eb5cc5273fbca93
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 17:07:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,13 @@ password = "AIzaSyCzmABOfcTbibbP_VmZPsFn0q3Ro2sXlHQ"
 response = requests.get('http://google.com', {'password': password})
 "AWS_ACCESS_KEY_ID": "AKIAUHD5AAQAAAAQAY1Z"
 amazonpassword = "amzn.mws.17abcde4-f3a4-c45b-c2dd-2a5e6c7d22ca"
-AzureToken: 'Y29weXJpZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Y29eeXJpZ2h0Y29weXJtZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Yw==' //AzureSecret
+AzureToken: 'Y29weXJpZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Y29eeXJpZ2h0Y29weXJtZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Yw==' #AzureSecret
 cosmosDbToken: Y29weXJpZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Y29eeXJpZ2h0Y29weXJtZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Yw==
 AzureCosmosPassword: Y29weXJpZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Y29eeXJpZ2h0Y29weXJtZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Yw==
 AzureAccountEndpoint=https://<database name>.documents.azure.com:443/;AccountKey=Y29weXJpZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Y29eeXJpZ2h0Y29weXJtZ2h0Y29weXJpZ2h0Y29weXJpZ2h0Yw==
 
 response = requests.get('https://todiaccount56.documents.azure.com:443', {'AccountKey': AzureToken})
 
+'access_token': 'dop_v1_89a302b0583901b006f70a6b755de666b0ec5441ada4fefcb46dec9934785b0b' #AccessToken
+digital_ocean_oauth = 'doo_v1_8476650cb0b595faa44a233f1d579b733248c0061f0d595a836cbce07de41854' #OAuth
+"refresh_token": "dor_v1_d6ce4b93104521c57be0b580e9296453ef4a319b02b5513469f0ec72d89af2e2" 
