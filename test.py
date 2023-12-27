4cc14ae3bc96b72c077c3a00a2614eeac040d188
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 14:03:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ import requests
 password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
-aws.secretaccesskeyid: AKIAUHD5AAQAAAAQAY1Z
+aws.secretaccesskeyid: "AKIAUHD5AAQAAAAQAY1Z"
+amazon = "amzn.mws.17abcde4-f3a4-c45b-c2dd-2a5e6c7d22ca"
