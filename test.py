dc8cc763ed1ffccb9b0992271f85148fa9aabe1b
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 17:50:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,12 @@ AzureAccountEndpoint=https://<database name>.documents.azure.com:443/;AccountKey
 response = requests.get('https://todiaccount56.documents.azure.com:443', {'AccountKey': AzureToken})
 
 DropboxAccessKey : sl.Bia6AMXdKF_m63pGFYe9Yuxgb59FyThPXKPxZaW-6zfsut08ZCcVf8ptT8m61zSk3qi_wMEMADO1J9Eyzz-Z5DaKH7P1y59FqX6VgStdAzV-OPSzcvFul1jPilpe69aFmUDXK75
+
+GITHUB_OAUTH_KEY= t6dafe7e502222499c285e35c20c01ad5789be12
+github_pat = "a9cbecdfdf953ec9b3b82dbc3b807cde2b06b445"
+"run git clone https://a9cbecdfdf953ec9b3b82dbc3b807cde2b06b445:a9dbebdfdf953ee9b3b82dbc3b807cde2b06b886@github.com/repo.git"
+"run git clone https://a9cbecdfdf953ec9b3b82dbc3b807cde2b06b445:x-oauth-basic@github.com/repo.git"
+"https://a9cbecdfdf953ec9b3b82dbc3b807cde2b06b445@github.com/repo.git"
+"git+https://neriuser:a9cbecdfct953cc9b3b82dbc3b807cde2b06b445@github.com/ReferenceME/RefMEStyleGuide.git#1.0.9"
+"https://a9deebdfdf953ee9b3b82gbc36787hde2b06b886:x-oauth-basic@github.com/ReferenceME/RefMEStyleGuide.git/sdf"
+"run git clone https://<username>:a9cbecdfct953cc9b3b86dbc3b807cde2b066445@github.com/doitintl/qwilt.git/"
