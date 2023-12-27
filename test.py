f9ac15506e638e5f107e966a5a1f169483e1eff6
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 13:43:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ import requests
 password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
+sms.aliyun.accesskey.secret=faKIMgFSfSwIhdKiun9Fn8Gmt8qhAo
+AccesskeyId: 'LTAI5tH5awSacaUtx5VqL9Am'
