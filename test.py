26c3d0f8bd39587d4f6c8a6d16d53c2bce68786f
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-27 13:39:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,3 @@ import requests
 password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
-adafruitkey = "aio_JflQ31LMsTtNIETnUR8dTBxHLda2"
