faaf23901798c572296139fd963df8bf2d9200f1
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-28 10:04:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -20,3 +20,7 @@ google_access_token : "ya29.AHES67zeEn-RDg9AC5gGKMLKuG4uVB7W4O4WjNr-NBfY6Dyer4vb
 'https://chat.googleapis.com/v1/spaces/AAAAH1W-wEM/messages?key=AIzaSyBbI0tEjhCZ6vySMm-WEfRttK3CPzttsHI&token=CalMPzURGJeIDs7yjM6V-nfK2xjoKYb297Omk_wI9Gs%3D'
 google_captcha = 'enc:system.encryption.key.v1:6Lgilge7ExVIvZPqmf1aPtFAh1WAQEQdEv5BCtua'
 google_oauth_access_token = "ya29.AHES52zeEn-RDg7CA9gGKMLKuG3uVB7W1O1WjNr-NBfY5Dtad3vbIZ"
+- text: |
+    {"installed":{"client_id":"332292536747-zbepioq1127w4lv5r6dbbddd0jdbdchl.apps.googleusercontent.com","project_id":"test-1551099712230","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"daAKAFW-bt05isOoQwwWP8c-","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+  client_id: 332292536747-zbepioq1127w4lv5r6dbbddd0jdbdchl.apps.googleusercontent.com
+  client_secret: daAKAFW-bt05isOoQwwWP8c-
