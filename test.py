e53d8836e791817fc11bb4e32ffdea4ec51b4fbc
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2023-12-28 14:10:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -50,9 +50,6 @@ jfrog_key = 'AKCp8oiHHT23xDyNaasrn3wwu1oXsHE4vcfcfudiGZU723EpM6KpNM3zEkan6ntKady
 - text: '-export NPM_TOKEN="npm_TBljNfh4TLQlHWVhybV4iXrsNj5bMQ9EMh6d"'
   apikey: npm_TBljNfh4TLQlHWVhybV4iXrsNj5bMQ9EMh6d
 
-- text: '+  stripe_secret_key: rk_live_epISNGSkdeXov2frTey7RHAi'
-  apikey: rk_live_epISNGSkdeXov2frTey7RHAi
-- text: '#   stripe_api_key: sk_live_EeHnL644i6zo4Iyq4v1KdV8H'
-  apikey: sk_live_EeHnL644i6zo4Iyq4v1KdV8H
-- text: 'stripe_api_key: sk_live_EeHnL644i6zo4Iyq4v1KdV9H'
+- text: 'stripe_wh_secret: whsec_VV1cfC0WFqnTPzHIAYUnnDS0t9g8I3Az'
+  apikey: whsec_VV1cfC0WFqnTPzHIAYUnnDS0t9g8I3Az
 
